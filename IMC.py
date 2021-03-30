2d2e638ab87d49641dece6aecefdabd270568518
Alan Rocha <alan.fernandes.rocha@gmail.com>
2021-03-29 23:19:18 -0300

    Update IMC.py

--- IMC.py
+++ IMC.py
@@ -1,27 +1,388 @@
-import os
- 
-altura = float(input("Digite sua altura em metros: "))
-peso = float(input("Digite seu peso em Kg: "))
- 
-imc = peso / altura**2
- 
-print("Seu IMC é: %.4f" % imc)
- 
-if imc < 16:
-	print("Magreza grave")
-elif imc < 17:
-	print("Magreza moderada")
-elif imc < 18.5:
-	print("Magreza leve")
-elif imc < 25:
-	print("Saudável")
-elif imc < 30:
-	print("Sobrepeso")
-elif imc < 35:
-	print("Obesidade Grau I")
-elif imc < 40:
-	print("Obesidade Grau II (severa)")
-else:
-	print("Obesidade Grau III (mórbida)")
- 
-os.system("pause")
+id = int(input("Digite a sua idade: "))
+a = float(input("Digite a sua altura(M): "))
+p = float(input("Digite o seu peso(Kg): "))
+s = str(input("Digita o seu sexo (M/F): "))
+imc = p / (a ** 2)
+
+
+if s == "M":
+    if id == 1:
+        if imc >= 19.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 18.2:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 14.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 2:
+        if imc >= 18.7:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.4:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 14:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 3:
+        if imc >= 18.2:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.4:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 4:
+        if imc >= 18:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 16.7:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.2:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 5:
+        if imc >= 18.1:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 16.7:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 6:
+        if imc >= 18.4:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 16.9:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.2:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 7:
+        if imc >= 18.7:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.2:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.3:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 8:
+        if imc >= 19.4:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.5:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 9:
+        if imc >= 20.1:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 18:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.6:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 10:
+        if imc >= 28.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 25:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 17.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 11:
+        if imc >= 22:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 19.3:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 14.2:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 12:
+        if imc >= 23:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 20:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 14.6:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 13:
+        if imc >= 24.2:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 21:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 15:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 14:
+        if imc >= 25.3:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 21.9:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 15.6:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 15:
+        if imc >= 26.4:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 22.8:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 16.2:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 16:
+        if imc >= 27.3:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 23.7:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 16.7:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 17:
+        if imc >= 28:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 24.5:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 17.2:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 18:
+        if imc >= 28.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 25:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 17.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id >= 19:
+        if imc >= 30:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 25:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 18.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+if s == "F":
+    if id == 1:
+        if imc >= 19.4:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.9:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.9:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 2:
+        if imc >= 18.5:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.2:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 3:
+        if imc >= 18.2:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 16.8:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.2:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 4:
+        if imc >= 18.3:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 16.8:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 5:
+        if imc >= 18.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 12.8:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 6:
+        if imc >= 18.9:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.1:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 12.8:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 7:
+        if imc >= 19.5:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.4:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 12.9:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 8:
+        if imc >= 20.1:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 17.8:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 9:
+        if imc >= 21:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 18.5:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.3:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 10:
+        if imc >= 22:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 19.1:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 13.6:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 11:
+        if imc >= 23.2:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 20:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 14:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 12:
+        if imc >= 24.4:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 20.9:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 14.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 13:
+        if imc >= 25.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 22:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 15.1:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 14:
+        if imc >= 26.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 22.9:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 15.6:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 15:
+        if imc >= 27.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 23.6:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 16.1:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 16:
+        if imc >= 28.3:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 24.3:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 16.4:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 17:
+        if imc >= 28.6:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 24.6:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 16.6:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id == 18:
+        if imc >= 28.9:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 24.9:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 16.7:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
+
+    if id >= 19:
+        if imc >= 30:
+            print("O seu IMC é {:.2f} e você tem obsidade I".format(imc))
+        elif imc >= 24.9:
+            print("O seu IMC é {:.2f} e você tem pré obsidade".format(imc))
+        elif imc >= 18.5:
+            print("O seu IMC é {:.2f} e você tem peso normal".format(imc))
+        else:
+            print("O seu IMC é {:.2f} e você tem magreza".format(imc))
